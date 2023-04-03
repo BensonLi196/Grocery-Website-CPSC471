b203c6faeebb70be71ad72b5073fd3a6dfe26bfc
Sefadugik <88416172+Sefadugik@users.noreply.github.com>
2023-04-03 17:13:16 -0600

    Update cpsc_fp_testing.sql

--- cpsc_fp_testing.sql
+++ cpsc_fp_testing.sql
@@ -1,8 +1,11 @@
--- items, user, supplier, store, manager, orders, manages
+-- items,user,supplier,store,manager,orders,manages,customer,browses,makes,has,manages,stocks,receives
+-- whats left are household, grocery, the shopping list things, and fixing some stuff for this code
+
 DROP DATABASE IF EXISTS GROCERY_STORE;
 CREATE DATABASE GROCERY_STORE; 
 USE GROCERY_STORE;
 
+-- creating a list of items to pick from, a table that contains all items as a single list
 DROP TABLE IF EXISTS ITEMS;
 CREATE TABLE ITEMS (
 	itemID		int not null,
@@ -13,6 +16,7 @@ CREATE TABLE ITEMS (
     CONSTRAINT item_pk PRIMARY KEY (itemID)
 );
 
+-- a database of all users who are signed up
 DROP TABLE IF EXISTS THE_USER;
 CREATE TABLE THE_USER (
 	userID		int not null,
@@ -23,6 +27,7 @@ CREATE TABLE THE_USER (
     CONSTRAINT the_user_pk PRIMARY KEY (userID)
 );
 
+-- a list of the stores suppliers
 DROP TABLE IF EXISTS SUPPLIER;
 CREATE TABLE SUPPLIER (
 	supID		int not null,
@@ -31,6 +36,7 @@ CREATE TABLE SUPPLIER (
     CONSTRAINT store_pk PRIMARY KEY (supID)
 );
 
+-- information of the store
 DROP TABLE IF EXISTS STORE;
 CREATE TABLE STORE (
 	storeID		int not null,
@@ -38,10 +44,13 @@ CREATE TABLE STORE (
     address		varchar(255) not null,
     openHours	varchar(255) not null,
     stocks		int,
+    employes	int,
     FOREIGN KEY (stocks) REFERENCES ITEMS(itemID), 
+    FOREIGN KEY (employes) REFERENCES MANAGER(mgrID),
     CONSTRAINT store_pk PRIMARY KEY (storeID)
 );
 
+-- manager and customer probably need to change
 DROP TABLE IF EXISTS MANAGER;
 CREATE TABLE MANAGER (
 	mgrID		int not null,
@@ -54,26 +63,67 @@ CREATE TABLE MANAGER (
 -- 	REFERENCES STORE(storeID)
 );
 
+-- probably needs to change
+DROP TABLE IF EXISTS CUSTOMER;
+CREATE TABLE CUSTOMER (
+	ctmrID		int not null,
+    FOREIGN KEY (ctmrID) REFERENCES THE_USER(userID)
+);
+
+-- allows user to browse through the items list
+DROP TABLE IF EXISTS BROWSES;
+CREATE TABLE BROWSES (
+	ctmrID		int not null,
+    itemID		int not null,
+    discount	varchar(45),
+    FOREIGN KEY (ctmrID) REFERENCES CUSTOMER(ctmrID),
+    FOREIGN KEY (itemID) REFERENCES ITEMS(itemID)
+);
+
+-- a list of deliverables, items that come from the supplier
+DROP TABLE IF EXISTS HAS;
+CREATE TABLE HAS (
+	itemID		int not null,
+    supID		int not null,
+    deliverAddr	varchar(45) not null,
+    FOREIGN KEY (itemID) REFERENCES ITEM(itemID),
+    FOREIGN KEY (supID) REFERENCES SUPPLIER(supID)
+);
+
+-- needs to become a weak entity
+-- not sure if the structure is correct in terms of containing multiple items per 1 order
 DROP TABLE IF EXISTS ORDERS;
 CREATE TABLE ORDERS (
 	orderID		int not null,
 	mgrID		int not null,
     supID		int not null,
+    items		int,
 --     CONSTRAINT fk_the_mgrID FOREIGN KEY (mgrID)
 -- 	REFERENCES THE_USER(userID),
 --     CONSTRAINT fk_supID FOREIGN KEY (supID)
 -- 	REFERENCES SUPPLIER(supID),
 	FOREIGN KEY (mgrID) REFERENCES THE_USER(userID),
     FOREIGN KEY (supID) REFERENCES SUPPLIER(supID),
+    FOREIGN KEY (items) REFERENCES ITEMS(itemID),
     CONSTRAINT orderID_pk PRIMARY KEY (orderID)
 );
 
+-- the items the manager can view/manage
 DROP TABLE IF EXISTS MANAGES;
 CREATE TABLE MANAGES (
 	mgrID		int,
     itemID		int,
     FOREIGN KEY (mgrID) REFERENCES THE_USER(userID),
     FOREIGN KEY (itemID) REFERENCES ITEMS(itemID)
+);
+
+-- also not sure if this is correct in terms of creating a single order with multiple items
+DROP TABLE IF EXISTS MAKES;
+CREATE TABLE MAKES (
+	mgrID		int not null,
+    items		int not null,
+    FOREIGN KEY (mgrID) REFERENCES MANAGER(magrID),
+    FOREIGN KEY (items) REFERENCES ITEMS(itemID)
 );
 
 DROP TABLE IF EXISTS BROWSES;
@@ -84,14 +134,24 @@ CREATE TABLE BROWSES (
     menu		varchar(45), -- ????
     FOREIGN KEY (userID) REFERENCES THE_USER(userID),
     FOREIGN KEY (itemID) REFERENCES ITEMS(itemID)
-    
 );
 
+DROP TABLE IF EXISTS RECEIVES;
+CREATE TABLE RECEIVES (
+	orderID		int,
+    supID		int,
+    FOREIGN KEY (orderID) REFERENCES ORDERS(orderID),
+    FOREIGN KEY (supID) REFERENCES SUPPLIER(supID)
+);
+
+-- unsure how to implement this so i left the structure here for later, feel free to work on it
 -- DROP TABLE IF EXISTS SHOPPING_LIST;
 -- CREATE TABLE SHOPPING_LIST (
 -- 	
 -- );
 
+-- here is where we can initially populate the databases, when you rerun the database (ie hit the lighting bolt again, all information
+-- that was changed in the program resets.
 INSERT INTO ITEMS (itemID, itemName, price, aisle, amount) 
 VALUES 
 	(0, "tomato", 3, "3", 10),
