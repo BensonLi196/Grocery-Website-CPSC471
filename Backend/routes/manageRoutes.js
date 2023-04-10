3439284a21abbade25586584b2ec8b4960aad045
Sefadugik <88416172+Sefadugik@users.noreply.github.com>
2023-04-10 16:41:28 -0600

    Manage item update

--- Backend/routes/manageRoutes.js
+++ Backend/routes/manageRoutes.js
@@ -2,12 +2,18 @@ const express = require("express");
 
 const {
     removeItem,
-    addGroceryItem
+    updateItem,
+    addGroceryItem,
+    addHouseholdItem,
+    addPharmacyItem
 } = require("../functions/ManageFunctions");
 
 const router = express.Router();
 
 router.delete("/removeItem", removeItem);
+router.patch("/update", updateItem);
 router.post("/addGrocery", addGroceryItem);
+router.post("/addHousehold", addHouseholdItem);
+router.post("/addPharmacy", addPharmacyItem);
 
 module.exports = router;
