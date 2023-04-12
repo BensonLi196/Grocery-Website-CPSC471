f6fbcf2a081d4822908f8210dd70874c74d86de0
Jack <jack.li2@ucalgary.ca>
2023-04-11 20:39:19 -0600

    oops

--- frontend/src/callAPI.js
+++ frontend/src/callAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const BASE_URL = "http://localhost:5000";
-const BASE_URL = "http://192.168.1.72:5000";
+const BASE_URL = "http://localhost:5000";
+
 ///////////////////////////////////////////////////USER APIS
 // Endpoint to login
 export const LoginAPI = async (Email, password) => {
